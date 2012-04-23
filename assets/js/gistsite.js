ea9c6dc2e8b11c0d08600f224274bc40cc308b65
Mastahyeti <mastahyeti@gmail.com>
2012-04-22 19:09:37 -0500

    everythings kinda workin

--- assets/js/gistsite.js
+++ assets/js/gistsite.js
@@ -7,35 +7,85 @@ var site_title_long = 'MastahYeti'
 var pages_prefix = /gistblog-sitepage/
 var blogpost_prefix = /gistblog-blogpost/
 
-var gistapi = 'https://api.github.com'
-var gistslisturl = gistapi + '/users/' + username + '/gists?callback=?'
+var gists_url = 'https://api.github.com/users/' + username + '/gists?callback=?'
+var gist_url = 'https://api.github.com/gists/'
+
+var homepage = '2467411'
+
 // END CONFIG
 
+var History = window.History
+var cache = {}
+
+// get a gist from github by its id
+function get_gist(id,callback){
+	if (id in cache){
+		callback(cache[id])
+	} else {
+		var url = gist_url + id + '?callback=?'
+		$.getJSON(url,function (response){
+			cache[id] = response
+			callback(response)
+		})
+	}
+}
+
+// fix our branding
+function fix_static_content(){
+	$('.site_title_short').html(site_title_short)
+	$('.site_title_long').html(site_title_long)
+}
+
+function set_active(){
+	hash = location.hash.slice(1)
+	if (hash == '')
+		hash = homepage
+	$('.active').removeClass('active')
+	$('.jslink#'+hash).parent().addClass('active')
+}
+
+function loading(){
+	console.log('loading')
+	$('.brand').html("<image src='assets/img/loading.gif'>")
+	$('.dropdown').hide()
+	$('.jslink').hide()
+}
+
+function done_loading(){
+	console.log('done loading')
+	$('.brand').html(site_title_short)
+	$('.dropdown').show()
+	$('.jslink').show()	
+}
+
 // handle the user clicking a link
-$('.pagelink').live('click',function(e){
-	e.preventDefault()
-	var url = e.currentTarget.href
-	$.getJSON(url,function(response){
-		if(response.meta.status != 200)
-			alert("ajax error")
-		else {
+$(window).on('hashchange', function(e){
+	// get id from location
+	id = location.hash.slice(1)
+	get_gist(id,function (response){
+		// make sure we own the gist (XSS protection)
+		if (response.data.user.login == username){
 			$.each(response.data.files,function(k,v){
 				$('#maincontent').html(v.content)
 			})
+			fix_static_content()
+			set_active()
 		}
 	})
 })
 
 $(document).ready(function(){
-	// our branding into the page
-	$('.site_title_short').html(site_title_short)
-	$('.site_title_long').html(site_title_long)
-
-	var pages = []
-	var posts = []
+	// fix branding
+	fix_static_content()
+	// if we are on our homepage, do homepage stuff
+	if (location.hash == '')
+		location.hash = homepage
+	
+	// display loading animation
+//	loading()
 
 	// sort out the pages and the posts
-	$.getJSON(gistslisturl,function(response){
+	$.getJSON(gists_url,function(response){
 		if (response.meta.status != 200)
 			alert('Ajax error')
 		else {
@@ -44,16 +94,21 @@ $(document).ready(function(){
 				if(response.data[index].description.match(pages_prefix)){
 					// do page stuff
 					var pagename = response.data[index].description.split(':').slice(1).join(':')
-					var url = response.data[index].url
-					console.log(pagename)
+					var id = response.data[index].id
 					// add page links
-					$('.nav').append("<li class='navelement'><a href='"+url+"?callback=?' class='pagelink'>"+pagename+"</a></li>")
+					$('.nav').append("<li class='navelement'><a href='#"+id+"' id='"+id+"' class='jslink'>"+pagename+"</a></li>")
 				}
 				if(response.data[index].description.match(blogpost_prefix)){
 					// do blogpost stuff
-					console.log(response.data[index].description)
+					var pagename = response.data[index].description.split(':').slice(1).join(':')
+					var id = response.data[index].id
+					// inject links
+					$('.dropdown-menu').append("<li class='navelement'><a href='#"+id+"' id='"+id+"' class='jslink'>"+pagename+"</a></li>")
 				}
 			})
 		}
+		set_active()
+		// hide loading animation
+//		done_loading()
 	})
 })
