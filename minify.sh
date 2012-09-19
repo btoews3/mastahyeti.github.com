c4b4494b7136e299372902cb898333b287981ae3
Ben Toews <btoews@Bens-MacBook-Pro.local>
2012-09-19 10:41:53 -0500

    updated minify script

--- minify.sh
+++ minify.sh
@@ -1,6 +1,15 @@
 #! /bin/sh
 
+# compile coffeescript
+cd assets/js
+coffee -c gistsite.coffee
+
 # minify js
-cat assets/js/showdown.js assets/js/gistsite.js | slimit -m > assets/js/common.js
+cat showdown.js gistsite.js | slimit -m > common.js
+
+cd ../css
+
 #minify css
-cat assets/css/bootstrap.css assets/css/gistsite.css assets/css/bootstrap-responsive.css | cssmin > assets/css/common.css
+cat bootstrap.css gistsite.css bootstrap-responsive.css | cssmin > common.css
+
+cd ../../
